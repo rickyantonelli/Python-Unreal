221e3e8d6ae4a397a781553b42942a8802b0182e
rickyantonelli <rickyantonelli@yahoo.com>
2024-06-24 13:44:27 -0700

    Continuing to expand on widget before incorporating Unreal - Can make cubes or spheres - Buttons to add mores to the view - General flexibility additions

--- graphicview.py
+++ graphicview.py
@@ -1,9 +1,9 @@
 import sys
-from PyQt6.QtCore import Qt, QPointF
-from PyQt6.QtGui import QPen, QBrush, QColor, QPainter
-from PyQt6.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsItem, QGraphicsEllipseItem, QMainWindow
+from PyQt6.QtCore import Qt, QPointF, QRectF
+from PyQt6.QtGui import QPen, QBrush, QColor, QPainter, QPolygonF
+from PyQt6.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, QGraphicsItem, QGraphicsEllipseItem, QMainWindow, QGraphicsRectItem, QGraphicsPolygonItem
 
-class DraggableAsset(QGraphicsEllipseItem):
+class DraggableItem(QGraphicsItem):
     def __init__(self, x, y, width, height):
         super().__init__(x, y, width, height)
         self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
@@ -12,6 +12,9 @@ class DraggableAsset(QGraphicsEllipseItem):
         self.setPen(QPen(Qt.GlobalColor.black))
         self.setAcceptHoverEvents(True)
         self.offset = QPointF(0, 0)
+        self.setPos(x, y)
+        self.width = width
+        self.height = height
 
     def mousePressEvent(self, event):
         self.offset = event.pos()
@@ -22,9 +25,20 @@ class DraggableAsset(QGraphicsEllipseItem):
         super().mouseReleaseEvent(event)
 
     def mouseMoveEvent(self, event):
+        # TODO: Currently can drag this off screen
         newPos = event.scenePos() - self.offset
         self.setPos(newPos.x(), newPos.y())
         super().mouseMoveEvent(event)
+        
+class DraggableSquare(QGraphicsRectItem, DraggableItem):
+    def __init__(self, x, y, width, height):
+        QGraphicsRectItem.__init__(self, QRectF(0, 0, width, height))
+        DraggableItem.__init__(self, x, y, width, height)
+        
+class DraggableEllipse(QGraphicsEllipseItem, DraggableItem):
+    def __init__(self, x, y, width, height):
+        QGraphicsEllipseItem.__init__(self, QRectF(0, 0, width, height))
+        DraggableItem.__init__(self, x, y, width, height)
 
 class GridGraphicsView(QGraphicsView):
     def __init__(self):
@@ -33,13 +47,17 @@ class GridGraphicsView(QGraphicsView):
         self.setScene(self.scene)
         self.setRenderHint(QPainter.RenderHint.Antialiasing)
 
-    def createGrid(self, size, width, height):
+    def createGrid(self, size=20, width=800, height=600):
         lightGray = QColor(211, 211, 211)
         for x in range(0, width, size):
             self.scene.addLine(x, 0, x, height, QPen(lightGray))
         for y in range(0, height, size):
             self.scene.addLine(0, y, width, y, QPen(lightGray))
         
-    def addAsset(self, posX, posY, width, height):
-        asset = DraggableAsset(posX, posY, width, height)
+    def addAsset(self, shape='square', posX=0, posY=0, width=15, height=15):
+        if shape == 'circle':
+            asset = DraggableEllipse(posX, posY, width, height)
+        else:
+            asset = DraggableSquare(posX, posY, width, height)
         self.scene.addItem(asset)
+        
--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 from PyQt6.QtWidgets import *
-import qgraphicsview   
-
-class MainWindow(QMainWindow):
+import graphicview   
+        
+class GridWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("2D Grid")
-        self.setGeometry(100, 100, 800, 600)
-        self.view = qgraphicsview.GridGraphicsView()
-        self.setCentralWidget(self.view)
+        self.view = graphicview.GridGraphicsView()
+        
+        self.addCubeButton = QPushButton("Add Cube")
+        self.addSphereButton = QPushButton("Add Sphere")
+        
+        self.vertLayout = QVBoxLayout(self)
+        self.buttonLayout = QHBoxLayout()
+        
+        self.vertLayout.addWidget(self.view)
+        self.buttonLayout.addWidget(self.addCubeButton)
+        self.buttonLayout.addWidget(self.addSphereButton)
+        
+        self.vertLayout.addLayout(self.buttonLayout)
+        
+        self.setLayout(self.vertLayout)
+        
+        self.addCubeButton.pressed.connect(self.addCube)
+        self.addSphereButton.pressed.connect(self.addSphere)
+        
+    def addCube(self):
+        print('adding cube')
+        self.view.addAsset('square', 0, 0, 15, 15)
+        
+    def addSphere(self):
+        gridWidget.view.addAsset('circle', 20, 20, 15, 15)
 
 if __name__ == '__main__':
     import sys
 
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = QMainWindow()
+    window.setWindowTitle("Unreal Blockout Widget")
+    gridWidget = GridWidget()
+    window.setCentralWidget(gridWidget)
     
-    window.view.createGrid(20, 800, 600)
-    window.view.addAsset(0, 0, 15, 15)
-    window.view.addAsset(20, 20, 15, 15)
+    gridWidget.view.createGrid(20, 800, 600)
+    gridWidget.view.addAsset('square', 0, 0, 15, 15)
+    gridWidget.view.addAsset('circle', 20, 20, 15, 15)
     
     window.show()
     sys.exit(app.exec())
